9ed5711fadcc16f0fa3ff099e555e169646df4b9
husseinlatif <hussein.latif@poli.ufrj.br>
2021-04-18 19:16:53 -0300

    commit inicial

---
+++ include/Grafo.h
@@ -0,0 +1,14 @@
+using namespace std;
+
+class Graph {
+    public:
+        Graph();
+        virtual ~Graph();
+
+
+    protected:
+        int nVertices;
+        int nArestas;
+
+        
+}
---
+++ src/Grafo.cpp
@@ -0,0 +1,13 @@
+#include <Graph.h>
+
+using namespace std;
+
+Graph(){
+
+
+}
+
+~Graph(){
+
+    
+}
